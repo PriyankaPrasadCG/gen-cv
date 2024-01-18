b1beb3feae4b4140c3048c6b08a9ed726dcc00f5
Arun Sahu <arun.sahu@capgemini.com>
2024-01-18 06:18:38 +0530

    added inside code app insights

--- avatar/interactive/api/message/__init__.py
+++ avatar/interactive/api/message/__init__.py
@@ -16,7 +16,8 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chat_models import AzureChatOpenAI
 from langchain_community.vectorstores import FAISS
-from appinsight_logger import app_logger
+import logging
+from applicationinsights import TelemetryClient
 
 search_endpoint = os.getenv("AZURE_SEARCH_ENDPOINT")
 search_key = os.getenv("AZURE_SEARCH_API_KEY") 
@@ -44,6 +45,38 @@ blue, end_blue = '\033[36m', '\033[0m'
 
 place_orders = False
 
+##############################
+#Arun - Application insights
+# Set the Application Insights Instrumentation Key
+instrumentation_key = "1cbd322a-9f07-4eb1-9ad0-db7e89a9815f"
+os.environ["APPINSIGHTS_INSTRUMENTATIONKEY"] = instrumentation_key
+
+# Initialize the TelemetryClient
+tc = TelemetryClient(instrumentation_key)
+
+# Set up logging to use Application Insights
+class ApplicationInsightsHandler(logging.Handler):
+    def __init__(self, instrumentation_key, *args, **kwargs):
+        self.instrumentation_key = instrumentation_key
+        super().__init__(*args, **kwargs)
+
+    def emit(self, record):
+        msg = self.format(record)
+        tc.track_trace(msg)
+        tc.flush()
+
+# Set up the logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Add the Application Insights handler to the logger
+ai_handler = ApplicationInsightsHandler(instrumentation_key)
+logger.addHandler(ai_handler)
+
+# Export the logger for use in other files
+app_logger = logger
+
+#################################
 functions = [
                 {
                     "name": "get_bonus_points",
